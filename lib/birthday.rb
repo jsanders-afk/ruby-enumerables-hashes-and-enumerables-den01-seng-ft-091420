6f545b74e395bdaaf4b52c0e6b17b9f8607c33ba
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 06:47:05 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,8 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  happy_bithday.each do |kid, age|
-  puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    happy_bithday.each do |kid, age|
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
 end
 binding.pry
