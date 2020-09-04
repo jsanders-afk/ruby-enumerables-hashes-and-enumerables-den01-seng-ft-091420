0b712414781c91c5afa57482f9367eec4f6c3bcc
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 06:41:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,9 +10,10 @@ suite_e: "Crumpet the Elf"
 
 def select_winner(passengers)
 winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
+  passengers.each do |suite, name|
+   if suite == :suite_a && name.start_with?("A")
     winner = name
+   end
   end
 end
 binding.pry
